b7a45122f552cbb975988ef7834c15074cb8fecd
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-30 16:17:19 +0330

    Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    var highNum: number = haystack.length;
+    var lowNum: number = 0;
+    var midNum: number = Number.NaN;
 
+    while (lowNum < highNum) {
+        // Supper dupper important the `Math.floor()`
+        midNum = Math.floor((highNum + lowNum) / 2);
+        if (haystack[midNum] === needle) return true;
+        else if (needle > haystack[midNum]) lowNum = midNum + 1;
+        else if (needle < haystack[midNum]) highNum = midNum;
+    }
+    return false;
 }
