d00722e805f14d4c71369408671970068144e3e1
rezishon <rezashahmoradi.81@outlook.com>
2025-05-02 23:51:00 +0330

    fix(Bubble sort): make replace function more clear

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -2,14 +2,14 @@ export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                replace(j, arr);
+                replace(j + 1, j, arr);
             }
         }
     }
 }
 
-function replace(j: number, arr: number[]): void {
-    let temp = arr[j + 1];
-    arr[j + 1] = arr[j];
+function replace(i: number, j: number, arr: number[]): void {
+    let temp = arr[i];
+    arr[i] = arr[j];
     arr[j] = temp;
 }
