c600adeea01f16fbb6908fd1572365233b0609b0
Rezishon <rezashahmoradi.81@outlook.com>
2025-05-10 18:22:06 +0330

    feat(Recursion): handle movments off the map
    it shouldn't goes off the map.

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -6,6 +6,15 @@ function walk(
     seen: boolean[][],
 ): boolean {
     // 1. Base case
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
 }
 export default function solve(
     maze: string[],
