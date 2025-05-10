f12d1ee98481b5ed3768fa073a4d45cd59185019
Rezishon <rezashahmoradi.81@outlook.com>
2025-05-10 18:21:01 +0330

    feat(Recursion): add walk function
    This function will help us move in our map which is a 2D string array.

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,11 @@
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+}
 export default function solve(
     maze: string[],
     wall: string,
